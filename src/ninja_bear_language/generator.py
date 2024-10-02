367dcffba4ddecfd7b23b5e95afcc6c9cacf06f3
monstermichl <vouillarmet.michel@gmail.com>
2024-10-02 13:33:20 +0200

    Implement generator

--- src/ninja_bear_language/generator.py
+++ src/ninja_bear_language/generator.py
@@ -1,17 +1,91 @@
+from enum import Enum
 from typing import List
-from ninja_bear import GeneratorBase, Property, NamingConventionType
+from ninja_bear import GeneratorBase, Property, PropertyType, NamingConventionType
+from ninja_bear.base.generator_configuration import GeneratorConfiguration
+
+
+class ExportType(str, Enum):
+    ESM = 'esm'
+    COMMON_JS = 'common_js'
+    NONE = 'none'
+
+
+class UnknownExportTypeException(Exception):
+    def __init__(self, export_type: str):
+        super().__init__(f'Unknown export type {export_type}')
 
 
 class Generator(GeneratorBase):
     """
     JavaScript specific generator. For more information about the generator methods, refer to GeneratorBase.
     """
+    _ATTRIBUTE_EXPORT = 'export'
+
+    def __init__(self, config: GeneratorConfiguration, properties: List[Property] = ..., additional_props=...):
+        super().__init__(config, properties, additional_props)
+
+        # Evaluate which export type to use.
+        self.export_type = self._evaluate_export_type()
 
     def _default_type_naming_convention(self) -> NamingConventionType:
-        raise Exception('_default_type_naming_convention method not implemented')
+        return NamingConventionType.PASCAL_CASE
     
     def _line_comment(self, string: str) -> str:
-        raise Exception('_line_comment method not implemented')
+        return f'// {string}'
     
     def _dump(self, type_name: str, properties: List[Property]) -> str:
-        raise Exception('_dump method not implemented')
+        # Export class only directly if ESM is used.
+        code = 'export ' if self.export_type == ExportType.ESM else ''
+
+        # Start type definition.
+        code += f'{self._type_start(type_name)}\n'
+
+        # Create properties.
+        for property in properties:
+            type = property.type
+
+            if type == PropertyType.BOOL:
+                value = 'true' if property.value else 'false'
+            elif type == PropertyType.INT or type == PropertyType.FLOAT or type ==  PropertyType.DOUBLE:
+                value = property.value
+            elif type == PropertyType.STRING:
+                value = property.value.replace('\\', '\\\\')  # TODO: Might need to be refined.
+                value = f'\'{value}\''  # Wrap in single quotes.
+            elif type == PropertyType.REGEX:
+                value = f'/{property.value}/'  # Wrap in single quotes.
+            else:
+                raise Exception('Unknown type')
+
+            code += f'{' ' * self._indent}{self._property(property.name, value)}\n'
+
+        # End type definition.
+        code += self._type_end()
+
+        # Add module export only if CommonJS is used.
+        return code + (f'\nmodule.exports = {self._type_name}' if self.export_type == ExportType.COMMON_JS else '')
+
+    def _type_start(self, type_name: str):
+        return f'class {type_name} {{'
+    
+    def _property(self, name: str, value: str):
+        # Realize JavaScript constant by defining a Getter.
+        return f'static get {name}() {{ return {value}; }}'
+    
+    def _type_end(self) -> str:
+        return '}'
+
+    def _evaluate_export_type(self) -> ExportType:
+        export_type = ExportType.ESM  # Default to ESM.
+
+        if self._ATTRIBUTE_EXPORT in self._additional_props:
+            exception_type_string = self._additional_props[self._ATTRIBUTE_EXPORT]
+
+            if exception_type_string == ExportType.ESM:
+                export_type = ExportType.ESM
+            elif exception_type_string == ExportType.COMMON_JS:
+                export_type = ExportType.COMMON_JS
+            elif exception_type_string == ExportType.NONE:
+                export_type = ExportType.NONE
+            else:
+                raise UnknownExportTypeException(exception_type_string)
+        return export_type
---
+++ tests/compare_files/test_config.js
@@ -0,0 +1,11 @@
+// Generated with ninja-bear v1.0.0 (https://pypi.org/project/ninja-bear/).
+
+export class TestConfig {
+    static get MyBoolean() { return true; }
+    static get MyInteger() { return 142; }
+    static get MyFloat() { return 322.0; }
+    static get MyCombinedFloat() { return 45724.0; }
+    static get MyDouble() { return 233.9; }
+    static get MyRegex() { return /Test Reg(E|e)x/; }
+    static get MySubstitutedString() { return 'Sometimes I just want to scream Hello World!'; }
+}
